0cec4acd6b2924aa0112e41b4420d9c8b22868de
Adubbz <Adubbz@users.noreply.github.com>
2018-02-23 00:13:04 +1100

    Use builtin fabs

--- common/math.h
+++ common/math.h
@@ -5,9 +5,5 @@
 float approxSin(float x);
 
 inline float clamp(float x, float min, float max) {
-    return x<min ? min : x>max ? max : x;
-}
-
-inline float nxfabs(float x) {
-    return x < 0 ? -x : x;
+    return x<min ? min : x > max ? max : x;
 }
