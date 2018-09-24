b9281534cc80bed36017e704135e7450440270e2
NightlyFox <nightlyfox94@gmail.com>
2018-09-24 16:18:52 -0500

    updated language for theme menu

--- common/language.c
+++ common/language.c
@@ -322,11 +322,11 @@ const char* const g_strings[StrId_Max][16] =
     
     [StrId_Actions_Theme_Menu] =
     {
-        STR_EN("Theme Switcher V1.0"),
-        STR_ES("Versión 1,0 del cambiador de tema"),
-        STR_JP("テーマスイッチャー版1.0"),
-        STR_KO("테마 스위처 에디션 1.0"),
-        STR_TW("主题切换器版本1。0"),
+        STR_EN("Theme Menu V1.0"),
+        STR_ES("Tema menu edición 1,0"),
+        STR_JP("テーマメニュー版1.0"),
+        STR_KO("테마 메뉴 에디션 1.0"),
+        STR_TW("主题菜单编辑1。0"),
     },
     /*[StrId_Reboot] =
     {
