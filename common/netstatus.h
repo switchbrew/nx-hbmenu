3ea2af46a540f2907ebb5365b1d490780756ee43
yellows8 <yellows8@users.noreply.github.com>
2019-06-22 17:47:31 -0400

    Added netstatus files which were missing from e917589. Closes #86.

---
+++ common/netstatus.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "common.h"
+
+bool netstatusGetDetails(AssetId *id);
+
---
+++ pc_main/pc_netstatus.c
@@ -0,0 +1,6 @@
+#include "../common/common.h"
+
+bool netstatusGetDetails(AssetId *id) {
+    *id = AssetId_wifi3_icon;
+    return true;
+}
