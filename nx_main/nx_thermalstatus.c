e0d7da0794a5b5b74339751f09bc2ca71161af84
Michael Scire <SciresM@gmail.com>
2023-10-20 15:24:37 -0700

    Update ts for 17.0.0

--- nx_main/nx_thermalstatus.c
+++ nx_main/nx_thermalstatus.c
@@ -1,14 +1,34 @@
 #include "../common/common.h"
 
+static TsSession g_tsInternalSession;
+
 bool thermalstatusInit(void) {
-    return R_SUCCEEDED(tsInitialize());
+    if (R_FAILED(tsInitialize()))
+        return false;
+
+    if (hosversionAtLeast(17,0,0) && R_FAILED(tsOpenSession(&g_tsInternalSession, TsDeviceCode_LocationInternal)))
+        return false;
+
+    return true;
 }
 
 void thermalstatusExit(void) {
+    if (hosversionAtLeast(17,0,0))
+        tsSessionClose(&g_tsInternalSession);
     tsExit();
 }
 
 bool thermalstatusGetDetails(s32 *temperature) {
-    return R_SUCCEEDED(tsGetTemperature(TsLocation_Internal, temperature));
+    if (hosversionAtLeast(17,0,0)) {
+        float temp_float;
+        if (R_SUCCEEDED(tsSessionGetTemperature(&g_tsInternalSession, &temp_float))) {
+            *temperature = (int)temp_float;
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return R_SUCCEEDED(tsGetTemperature(TsLocation_Internal, temperature));
+    }
 }
 
